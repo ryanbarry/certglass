dbc5a81e72dfca81cad236e7602752efc310f036
Ryan Barry <ryan@nuclearice.com>
2020-06-07 12:22:56 -0700

    refactor to be nicer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 fn main() {
-    let leaf1raw = include_bytes!("../lets_encrypt_ct_leaf2.dat");
-    let leaf1extraraw = include_bytes!("../lets_encrypt_ct_leaf2extra.dat");
-    if let Ok(leaf1) = ctclient::internal::Leaf::from_raw(leaf1raw, leaf1extraraw) {
-        if !leaf1.is_pre_cert {
-            for cert in leaf1.x509_chain {
-                if let Ok(x509) = openssl::x509::X509::from_der(&cert) {
-                    if let Ok(names) = ctclient::certutils::get_common_names(&x509) {
-                        println!("common names: {:?}", names);
-                    } else {
-                        println!("got error getting cert common names");
-                    }
-                } else {
-                    println!("got error doing from_der");
-                }
-            }
-        }else {
-            println!("it's a pre-cert!");
+    let leafinputdat = include_bytes!("../lets_encrypt_ct_leaf2.dat");
+    let leafextradat = include_bytes!("../lets_encrypt_ct_leaf2extra.dat");
+
+    let leaf = match ctclient::internal::Leaf::from_raw(leafinputdat, leafextradat) {
+        Ok(l) => l,
+        Err(e) => {
+            println!("error parsing from raw: {}", e);
+            return;
         }
-    } else {
-        println!("couldn't parse");
+    };
+
+    if leaf.is_pre_cert {
+        println!("it's a precert.");
+        return;
+    }
+
+    for cert in leaf.x509_chain {
+        if let Ok(x509) = openssl::x509::X509::from_der(&cert) {
+            if let Ok(names) = ctclient::certutils::get_common_names(&x509) {
+                println!("common names: {:?}", names);
+            } else {
+                println!("got error getting cert common names");
+            }
+        } else {
+            println!("got error doing from_der");
+        }
     }
 }
