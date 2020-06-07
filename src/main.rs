c64388c0dc1766c63b124e1e0e508b70e9f626ce
Ryan Barry <ryan@nuclearice.com>
2020-06-07 16:56:03 -0700

    refactor further

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod merkle_tree_leaf;
 
-use merkle_tree_leaf::cert_from_leaf;
+use merkle_tree_leaf::TimestampedEntryData;
 
 fn main() {
-    let leafinputdat = include_bytes!("../lets_encrypt_ct_leaf1.dat");
-    let leafextradat = include_bytes!("../lets_encrypt_ct_leaf1extra.dat");
+    let leafinputdat = include_bytes!("../lets_encrypt_ct_leaf0.dat");
+    let leafextradat = include_bytes!("../lets_encrypt_ct_leaf0extra.dat");
 
-    let cert = match cert_from_leaf(leafinputdat, leafextradat) {
+    let tsedat = match TimestampedEntryData::from_raw(leafinputdat, leafextradat) {
         Ok(c) => c,
         Err(e) => {
             println!("error parsing cert from MerkleTreeLeaf: {:?}", e);
@@ -14,42 +14,5 @@ fn main() {
         }
     };
 
-    println!(
-        "common names in cert: {:?}",
-        ctclient::certutils::get_common_names(&cert).unwrap()
-    );
-    println!(
-        "signature alg: {}\nsignature_len: {}",
-        cert.signature_algorithm()
-            .object()
-            .nid()
-            .short_name()
-            .unwrap(),
-        cert.signature().len()
-    );
-
-    let leaf = match ctclient::internal::Leaf::from_raw(leafinputdat, leafextradat) {
-        Ok(l) => l,
-        Err(e) => {
-            println!("error parsing from raw: {}", e);
-            return;
-        }
-    };
-
-    if leaf.is_pre_cert {
-        println!("it's a precert.");
-        return;
-    }
-
-    for cert in leaf.x509_chain {
-        if let Ok(x509) = openssl::x509::X509::from_der(&cert) {
-            if let Ok(names) = ctclient::certutils::get_common_names(&x509) {
-                println!("common names: {:?}", names);
-            } else {
-                println!("got error getting cert common names");
-            }
-        } else {
-            println!("got error doing from_der");
-        }
-    }
+    println!("common names in cert: {:?}", tsedat.Subject);
 }
--- src/merkle_tree_leaf.rs
+++ src/merkle_tree_leaf.rs
@@ -25,90 +25,99 @@ pub enum Error {
     MalformedLeafInput(String),
 }
 
-pub fn cert_from_leaf(leaf_input: &[u8], extra_data: &[u8]) -> Result<openssl::x509::X509, Error> {
-    /*
-     type MerkleTreeLeaf struct {
-       Version          Version           `tls:"maxval:255"`
-       LeafType         MerkleLeafType    `tls:"maxval:255"`
-       TimestampedEntry *TimestampedEntry `tls:"selector:LeafType,val:0"`
-     }
-    */
-    let err_invalid = Err(Error::MalformedLeafInput("Invalid leaf data.".to_owned()));
-    if leaf_input.len() < 2 {
-        return err_invalid;
-    }
-    let mut leaf_slice = &leaf_input[..];
-    let version = u8::from_be_bytes([leaf_slice[0]]);
-    let leaf_type = u8::from_be_bytes([leaf_slice[1]]);
-    if version != 0 || leaf_type != 0 {
-        return err_invalid; // TODO should ignore.
-    }
-    leaf_slice = &leaf_slice[2..];
-    /*
-      type TimestampedEntry struct {
-        Timestamp    uint64
-        EntryType    LogEntryType   `tls:"maxval:65535"`
-        X509Entry    *ASN1Cert      `tls:"selector:EntryType,val:0"`
-        PrecertEntry *PreCert       `tls:"selector:EntryType,val:1"`
-        JSONEntry    *JSONDataEntry `tls:"selector:EntryType,val:32768"`
-        Extensions   CTExtensions   `tls:"minlen:0,maxlen:65535"`
-      }
-    */
-    if leaf_slice.len() < 8 + 2 {
-        return err_invalid;
-    }
-    let timestamp = u64::from_be_bytes(leaf_slice[0..8].try_into().unwrap()); // unix epoch millis
+pub struct TimestampedEntryData {
+    pub Subject: Vec<String>,
+    pub Alternate: Vec<String>,
+    pub Timestamp: u64,
+}
 
-    leaf_slice = &leaf_slice[8..];
-    let entry_type = u16::from_be_bytes([leaf_slice[0], leaf_slice[1]]);
+fn err_invalid() -> Error {
+    Error::MalformedLeafInput("Invalid leaf data.".to_string())
+}
 
-    leaf_slice = &leaf_slice[2..];
-    match entry_type {
-        0 => {
-            // x509_entry
-            // len is u24
-            if leaf_slice.len() < 3 {
-                return err_invalid;
-            }
-            let len = u32::from_be_bytes([0, leaf_slice[0], leaf_slice[1], leaf_slice[2]]);
-            leaf_slice = &leaf_slice[3..];
-            if leaf_slice.len() < len as usize {
-                return err_invalid;
-            }
-            let x509_end = &leaf_slice[..len as usize]; // DER certificate
-
-            return match openssl::x509::X509::from_der(x509_end) {
-                Ok(c) => Ok(c),
-                Err(e) => Err(Error::OpenSSLErrorStack(e)),
-            };
+impl TimestampedEntryData {
+    pub fn from_raw(leaf_input: &[u8], extra_data: &[u8]) -> Result<Self, Error> {
+        /*
+         type MerkleTreeLeaf struct {
+           Version          Version           `tls:"maxval:255"`
+           LeafType         MerkleLeafType    `tls:"maxval:255"`
+           TimestampedEntry *TimestampedEntry `tls:"selector:LeafType,val:0"`
+         }
+        */
+        if leaf_input.len() < 2 {
+            return Err(err_invalid());
         }
-        1 => {
-            // precert_entry
-            /*
-             type PreCert struct {
-               IssuerKeyHash  [sha256.Size]byte
-               TBSCertificate []byte `tls:"minlen:1,maxlen:16777215"` // DER-encoded TBSCertificate
-             }
-            */
-            let mut extradat_slice = &extra_data[..];
-            if extradat_slice.len() < 3 {
-                return err_invalid;
-            }
-            let len =
-                u32::from_be_bytes([0, extradat_slice[0], extradat_slice[1], extradat_slice[2]]);
-            extradat_slice = &extradat_slice[3..];
-            if extradat_slice.len() < len as usize {
-                return err_invalid;
-            }
-            let tbs_data = &extradat_slice[..len as usize];
+        let mut leaf_slice = &leaf_input[..];
+        let version = u8::from_be_bytes([leaf_slice[0]]);
+        let leaf_type = u8::from_be_bytes([leaf_slice[1]]);
+        if version != 0 || leaf_type != 0 {
+            return Err(err_invalid()); // TODO should ignore.
+        }
+        leaf_slice = &leaf_slice[2..];
+        /*
+         type TimestampedEntry struct {
+           Timestamp    uint64
+           EntryType    LogEntryType   `tls:"maxval:65535"`
+           X509Entry    *ASN1Cert      `tls:"selector:EntryType,val:0"`
+           PrecertEntry *PreCert       `tls:"selector:EntryType,val:1"`
+           JSONEntry    *JSONDataEntry `tls:"selector:EntryType,val:32768"`
+           Extensions   CTExtensions   `tls:"minlen:0,maxlen:65535"`
+         }
+         type PreCert struct {
+           IssuerKeyHash  [sha256.Size]byte
+           TBSCertificate []byte `tls:"minlen:1,maxlen:16777215"` // DER-encoded TBSCertificate
+         }
+        */
 
-            return match openssl::x509::X509::from_der(tbs_data) {
-                Ok(c) => Ok(c),
-                Err(e) => Err(Error::OpenSSLErrorStack(e)),
-            };
+        if leaf_slice.len() < 8 + 2 {
+            return Err(err_invalid());
         }
-        _ => {
-            return err_invalid; // TODO should ignore.
+        let timestamp = u64::from_be_bytes(leaf_slice[0..8].try_into().unwrap()); // unix epoch millis
+
+        leaf_slice = &leaf_slice[8..];
+        let entry_type = u16::from_be_bytes([leaf_slice[0], leaf_slice[1]]);
+
+        leaf_slice = &leaf_slice[2..];
+        let parse_result: Result<openssl::x509::X509, Error> = match entry_type {
+            0 => parse_der(leaf_slice),
+            1 => parse_der(extra_data),
+            _ => Err(err_invalid()), // TODO should ignore.
+        };
+
+        match parse_result {
+            Ok(cert) => {
+                let sn = cert
+                    .subject_name()
+                    .entries_by_nid(openssl::nid::Nid::COMMONNAME)
+                    .map(|x| x.data().as_utf8())
+                    .filter_map(|x| x.ok())
+                    .map(|x| String::from(AsRef::<str>::as_ref(&x)))
+                    .collect();
+                Ok(TimestampedEntryData {
+                    Timestamp: timestamp,
+                    Subject: sn,
+                    Alternate: [].to_vec(),
+                })
+            }
+            Err(e) => Err(e),
         }
+    }
+}
+
+fn parse_der(data: &[u8]) -> Result<openssl::x509::X509, Error> {
+    if data.len() < 3 {
+        return Err(err_invalid());
+    }
+    let len = u32::from_be_bytes([0, data[0], data[1], data[2]]);
+
+    let dataslice = &data[3..];
+    if dataslice.len() < len as usize {
+        return Err(err_invalid());
+    }
+    let x509_data = &dataslice[..len as usize];
+
+    match openssl::x509::X509::from_der(x509_data) {
+        Ok(c) => Ok(c),
+        Err(e) => Err(Error::OpenSSLErrorStack(e)),
     }
 }
